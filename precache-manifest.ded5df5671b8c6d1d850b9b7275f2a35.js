80df8722d9ac98e18667e8a93d24ddb4ea9da179
aliamk <alia.azarian@gmail.com>
2020-10-10 18:01:46 +0100

    Updates

---
+++ precache-manifest.ded5df5671b8c6d1d850b9b7275f2a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1231874e6b773f50fbe2586749cecc",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "a0e3e8f61d30b9685e7f",
+    "url": "/covid_tracker/static/css/main.dd54b621.chunk.css"
+  },
+  {
+    "revision": "f3c1de17401ff5948cfb",
+    "url": "/covid_tracker/static/js/2.3dbaf27e.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_tracker/static/js/2.3dbaf27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e3e8f61d30b9685e7f",
+    "url": "/covid_tracker/static/js/main.64f40b4b.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
